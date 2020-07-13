5e2e25c276e448eabaca9230cc7bbcfc6fef48b2
Zion Sandeep <59429929+zion-s@users.noreply.github.com>
2020-07-13 19:55:08 +0530

    Updates

---
+++ precache-manifest.5f820af4524cf950a026cbb673e3c920.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2abf2086869755be3ae86cf07140b3",
+    "url": "/random-quote-machine-react/index.html"
+  },
+  {
+    "revision": "8e30a6e6048e5b6659b4",
+    "url": "/random-quote-machine-react/static/css/main.52e1b911.chunk.css"
+  },
+  {
+    "revision": "efd063ad88c7db373e20",
+    "url": "/random-quote-machine-react/static/js/2.cd6561e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine-react/static/js/2.cd6561e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e30a6e6048e5b6659b4",
+    "url": "/random-quote-machine-react/static/js/main.7eb1c544.chunk.js"
+  },
+  {
+    "revision": "55e5bacf2ffb8c56d126",
+    "url": "/random-quote-machine-react/static/js/runtime-main.3ed83743.js"
+  },
+  {
+    "revision": "7e7a3e2b41e40ae7439f09799bebca61",
+    "url": "/random-quote-machine-react/static/media/background.7e7a3e2b.jpg"
+  }
+]);
